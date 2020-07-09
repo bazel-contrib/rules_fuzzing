c5cc7aaaeb53754d8367b8719f804c4590d69adf
tengpeng <tengpeng.li2020@gmail.com>
2020-07-09 13:36:21 -0400

    delete launcher.py engine argument and use relative position to run executable

--- fuzzing/tools/launcher.py
+++ fuzzing/tools/launcher.py
@@ -5,14 +5,12 @@ This is the launcher script to provide a uniform command line interface behind a
 
 from absl import app
 from absl import flags
-from subprocess import call, TimeoutExpired
+from subprocess import call, TimeoutExpired, STDOUT
 import os
 
 FLAGS = flags.FLAGS
 
 flags.DEFINE_integer('timeout', 20, 'test timeout.', lower_bound=0)
-flags.DEFINE_enum('engine', 'libfuzzer', ['libfuzzer'],
-                  'the fuzzing engine used to do fuzzing test.')
 
 
 def main(argv):
@@ -20,10 +18,9 @@ def main(argv):
         raise app.UsageError("Too few arguments")
 
     ret_code = 0
-    cwd = os.getcwd()
     try:
         # Is this the right way to execute the runnable?
-        ret_code = call(cwd + "/../../../" + argv[1], timeout=FLAGS.timeout)
+        ret_code = call(argv[1], timeout=FLAGS.timeout)
     except TimeoutExpired as e:
         print("Error: Timeout")
     except Exception as e:
