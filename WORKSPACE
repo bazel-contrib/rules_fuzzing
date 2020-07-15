d7f7ddd07aa39a872494e6910b610e7013e172d0
Tengpeng <66138273+tengpengli@users.noreply.github.com>
2020-07-15 09:56:21 -0400

    Initial version of the Python launcher script for invoking fuzz target executables (#31)
    Initial version of the Python launcher script for invoking fuzz target executables.
    
    The script aims at providing an uniform command line interface behind a number of arbitrary fuzzing engines. For now the script perform a plain fuzzing run with a given timeout.
    
    The command to use this script looks like
    
    bazel run --config=asan-libfuzzer //examples:empty_fuzz_test --run_under='//fuzzing/tools:launcher --timeout_secs=5'
    
    or
    
    //fuzzing/tools:launcher EXECUTABLE --timeout_secs=5
    
    Signed-off-by: tengpeng <tengpeng.li2020@gmail.com>
    
    	modified:   fuzzing/tools/BUILD
    	modified:   fuzzing/tools/launcher.py

--- WORKSPACE
+++ WORKSPACE
@@ -12,3 +12,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+http_archive(
+    name = "rules_python",
+    url = "https://github.com/bazelbuild/rules_python/releases/download/0.0.2/rules_python-0.0.2.tar.gz",
+    strip_prefix = "rules_python-0.0.2",
+    sha256 = "b5668cde8bb6e3515057ef465a35ad712214962f0b3a314e551204266c7be90c",
+)
+load("@rules_python//python:repositories.bzl", "py_repositories")
+py_repositories()
---
+++ fuzzing/tools/launcher.py
@@ -0,0 +1,30 @@
+# Lint as: python3
+"""
+This is the launcher script to provide a uniform command line interface 
+behind a number of arbitrary fuzzing engines.
+"""
+
+from absl import app
+from absl import flags
+import os
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_integer(
+    'timeout_secs',
+    20,
+    'The maximum duration, in seconds, of the fuzzer run launched.',
+    lower_bound=0)
+
+
+def main(argv):
+    if len(argv) != 2:
+        raise app.UsageError(
+            "This script receives 1 argument. It should look like:" +
+            "\n\tpython " + __file__ + " EXECUTABLE")
+
+    os.execl(argv[1], argv[1], "-timeout=" + str(FLAGS.timeout_secs))
+
+
+if __name__ == '__main__':
+    app.run(main)
