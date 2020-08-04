2cd72332724c1b1a4eca9edbc85a5f46cc60567a
Tengpeng <66138273+tengpengli@users.noreply.github.com>
2020-08-03 20:45:25 -0400

    Fuzzing dictionary validation python script (#46)
    Fuzzing dictionary validation python script and its unit test
    
    Example usage:
            python validate_dict.py --dict_list=a.dict,b.dict --output_file=merged.dict
    
    The code models https://github.com/llvm/llvm-project/blob/6c67ee0f58323fe39db84635ef8cea629c2214b4/compiler-rt/lib/fuzzer/FuzzerUtil.cpp#L81
    
    Signed-off-by: tengpeng <tengpeng.li2020@gmail.com>
    
    	new file:   dict_validation.py
    	new file:   dict_validation_test.py
    	new file:   validate_dict.py
            modified:  fuzzing/tools/BUILD

---
+++ fuzzing/tools/dict_validation.py
@@ -0,0 +1,87 @@
+#
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""
+Validates the fuzzing dictionary.
+"""
+
+from string import hexdigits
+
+
+def validate_entry(entry):
+    """Validates a single fuzzing dictionary entry.
+
+    Args:
+        entry: a string containing a single entry.
+
+    Returns:
+        True if the argument is a valid fuzzing dictionary entry, 
+        otherwise False.
+    """
+
+    # Use set to contain hex digits to decrease the query time complexity
+    hex_set = set(hexdigits)
+    pos, end = 0, len(entry) - 1
+    while pos < end:
+        pos += 1
+        chr = entry[pos]
+
+        if not (chr.isprintable() or chr.isspace()):
+            return False
+
+        # Handle '\\'
+        if chr == '\\':
+            if pos + 1 <= end and (entry[pos + 1] == '\\' or
+                                   entry[pos + 1] == '"'):
+                pos += 1
+                continue
+
+            # Handle '\xAB'
+            if pos + 3 <= end and entry[pos + 1] == 'x' and entry[
+                    pos + 2] in hex_set and entry[pos + 3] in hex_set:
+                pos += 3
+                continue
+
+            return False
+
+    return True
+
+
+def validate_line(line):
+    """Validates a single line in the fuzzing dictionary entry.
+
+    Args:
+        line: a string containing a single line in the fuzzing dictionary.
+
+    Returns:
+        True if the argument is allowed to exist in a fuzzing dictionary, 
+        otherwise False.
+    """
+    line = line.strip()
+    if not line or line.startswith('#'):
+        return True
+    if len(line) < 2 or line[-1] != '"':
+        return False
+
+    left = 0
+    # Find the opening "
+    while left < len(line) - 1 and line[left] != '"':
+        left += 1
+
+    if left >= len(line) - 1:
+        return False
+
+    return validate_entry(line[left:])
---
+++ fuzzing/tools/dict_validation_test.py
@@ -0,0 +1,59 @@
+#
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""
+Unit tests for dict_validator.py
+"""
+
+import unittest
+from dict_validation import validate_line
+
+
+class DictValidatorTest(unittest.TestCase):
+
+    def test_plain_entries(self):
+        self.assertTrue(validate_line('kw1="blah"'))
+        self.assertTrue(validate_line('":path"'))
+        self.assertTrue(validate_line('"keep-alive"'))
+        self.assertTrue(validate_line('"te"'))
+        self.assertTrue(validate_line('"ab""'))
+
+    def test_escaped_words(self):
+        self.assertTrue(validate_line('kw2="\\"ac\\\\dc\\""'))
+        self.assertTrue(validate_line('kw3="\\xF7\\xF8"'))
+        self.assertTrue(validate_line('"foo\\x0Abar"'))
+
+    def test_invalid_escaped_words(self):
+        self.assertFalse(validate_line('"\\A"'))
+
+    def test_comment(self):
+        self.assertTrue(validate_line('# valid dictionary entries'))
+
+    def test_empty_string(self):
+        self.assertTrue(validate_line(''))
+
+    def test_spaces(self):
+        self.assertTrue(validate_line('   '))
+
+    def test_plain_words(self):
+        self.assertFalse(validate_line('Invalid dictionary entries'))
+
+    def test_single_quote(self):
+        self.assertFalse(validate_line('"'))
+
+
+if __name__ == '__main__':
+    unittest.main()
