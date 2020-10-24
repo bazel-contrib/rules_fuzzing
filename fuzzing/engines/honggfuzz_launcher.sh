b7f819e1dce9fee679547e8544686427370b179b
Stefan Bucur <sbucur@google.com>
2020-10-23 20:27:55 -0400

    Add Honggfuzz support.
    This includes importing and building Honggfuzz with Bazel, as well as
    defining a cc_fuzzing_engine specification for it.

---
+++ fuzzing/engines/honggfuzz_launcher.sh
@@ -0,0 +1,32 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+command_line=("${HONGGFUZZ_PATH}")
+command_line+=("--workspace=${FUZZER_OUTPUT_ROOT}")
+
+if [[ -n "${FUZZER_SEED_CORPUS_DIR}" ]]; then
+    command_line+=("--input=${FUZZER_SEED_CORPUS_DIR}")
+    command_line+=("--output=${FUZZER_OUTPUT_CORPUS_DIR}")
+else
+    command_line+=("--input=${FUZZER_OUTPUT_CORPUS_DIR}")
+fi
+
+command_line+=("--crashdir=${FUZZER_ARTIFACTS_DIR}")
+
+if [[ -n "${FUZZER_DICTIONARY_PATH}" ]]; then
+    command_line+=("--dict=${FUZZER_DICTIONARY_PATH}")
+fi
+command_line+=("--" "${FUZZER_BINARY}")
+
+exec "${command_line[@]}"
