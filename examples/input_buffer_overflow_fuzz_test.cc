955308ea8eb8dffc08e006fc79676855e9930fea
Stefan Bucur <sbucur@google.com>
2020-12-05 18:29:13 -0500

    Fix input buffer access in buffer overflow examples to guarantee overflow.

--- examples/input_buffer_overflow_fuzz_test.cc
+++ examples/input_buffer_overflow_fuzz_test.cc
@@ -18,7 +18,7 @@
 
 void TriggerBufferOverflow(const uint8_t *data, size_t size) {
   if (size >= 3 && data[0] == 'F' && data[1] == 'U' && data[2] == 'Z' &&
-      data[3] == 'Z') {
+      data[size] == 'Z') {
     fprintf(stderr, "BUFFER OVERFLOW!\n");
   }
 }
